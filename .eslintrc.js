12e6b02abd0c3d2547170e8d7af43b5e7946772f
April <April@Rain>
2020-01-25 20:48:05 +0000

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "parser": "babel-eslint",
   env: {
     browser: true,
     es6: true,
@@ -19,8 +20,8 @@ module.exports = {
     sourceType: "module",
   },
   plugins: [
-    "babel",
-    "react",
+   "react",
+  //  "babel",
   ],
   settings: {
     react: {
